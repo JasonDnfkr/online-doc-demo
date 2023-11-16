f0f889f265e791e96f2252ac59bf886d64247fb4
JasonDnfkr <jasondnfkr@qq.com>
2023-11-16 23:40:35 +0800

    modify back-end

--- src/main/java/com/example/online_doc/config/WebSocketConfig.java
+++ src/main/java/com/example/online_doc/config/WebSocketConfig.java
@@ -6,13 +6,29 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+/**
+ * 配置了使用 STOMP 协议的 WebSocket 服务器端点，
+ * 并通过 SockJS 提供了向后兼容性。
+ * 这允许客户端（如浏览器）通过 WebSocket 连接到 /shared 路径，并通过 STOMP 协议进行消息传递
+ */
 @Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    /**
+     * 用于注册 STOMP 端点。
+     * STOMP（Simple Text Oriented Messaging Protocol）是一种简单的文本导向的消息传递协议，
+     * 常用于 WebSocket 通信
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/shared").withSockJS();
     }
 
+    /**
+     * 该 Bean 是用于部署 WebSocket 服务器端点的
+     * 当使用 Spring 的 WebSocket 支持时，这个 Bean 会自动注册使用了 @ServerEndpoint 注解的类。
+     * @return
+     */
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
--- src/main/java/com/example/online_doc/config/WebSocketHandler.java
+++ src/main/java/com/example/online_doc/config/WebSocketHandler.java
@@ -14,8 +14,17 @@ import java.util.List;
 public class WebSocketHandler extends TextWebSocketHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketHandler.class);
 
+    /**
+     * 定义了一个静态列表 sessions，用于存储所有建立的 WebSocket 会话
+     * 允许服务器跟踪当前所有打开的 WebSocket 连接
+     */
     private static final List<WebSocketSession> sessions = new ArrayList<>();
 
+    /**
+     * 当收到客户端发送的消息时，服务器会记录这条消息，并将其转发给所有连接的会话（即广播消息）
+     * @param session
+     * @param message
+     */
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
         LOGGER.info("Received message: {}", message.getPayload());
@@ -28,11 +37,21 @@ public class WebSocketHandler extends TextWebSocketHandler {
         }
     }
 
+    /**
+     * 在 WebSocket 连接建立后被调用。
+     * 将新的会话添加到 sessions 列表中，这样就可以跟踪当前所有打开的连接
+     * @param session
+     */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         sessions.add(session);
     }
 
+    /**
+     * 当 WebSocket 连接关闭时，这个方法被调用。它从 sessions 列表中移除已关闭的会话。
+     * @param session
+     * @param status
+     */
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         sessions.remove(session);
